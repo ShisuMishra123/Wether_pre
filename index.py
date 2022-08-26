ed5ad9b71bc1d49c9c43832921bf5d25ff910ae8
ShisuMishra123 <112188755+ShisuMishra123@users.noreply.github.com>
2022-08-26 23:17:24 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,164 +1,167 @@
-import os
-import pytz
-import pyowm
 import streamlit as st
-from matplotlib import dates
-from datetime import datetime
-from matplotlib import pyplot as plt
-import plotly.graph_objects as go
-import plotly.express as px
+import datetime,requests
+from plotly import graph_objects as go
 
+st.set_page_config(page_title='Shisu Mishra', page_icon=":rainbow:")
 
+st.title("8-DAY WEATHER FORECAST 🌧️🌥️")
 
+city=st.text_input("ENTER THE NAME OF THE CITY ")
 
-degree_sign= u'\N{DEGREE SIGN}'
+unit=st.selectbox("SELECT TEMPERATURE UNIT ",["Celsius","Fahrenheit"])
 
-st.title("5 Day Weather Forecast")
-st.write("## Made by Shisu Mishra with ❤️")
+speed=st.selectbox("SELECT WIND SPEED UNIT ",["Metre/sec","Kilometre/hour"])
 
-st.write("### Write the name of a City and select the Temperature Unit and Graph Type from the sidebar")
+graph=st.radio("SELECT GRAPH TYPE ",["Bar Graph","Line Graph"])
 
-place=st.text_input("NAME OF THE CITY :", "")
+st.markdown(
+    """
+    <style>
+    .reportview-container {
+        background: url("https://wallpaperaccess.com/full/1442216.jpg")
+    }
+  
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
-
-if place == None:
-    st.write("Input a CITY!")
-
-
-
-unit=st.selectbox("Select Temperature Unit",("Celsius","Fahrenheit"))
-
-g_type=st.selectbox("Select Graph Type",("Line Graph","Bar Graph"))
-
-if unit == 'Celsius':
-    unit_c = 'celsius'
+if unit=="Celsius":
+    temp_unit=" °C"
 else:
-    unit_c = 'fahrenheit'
-
-
-def get_temperature():
-    days = []
-    dates = []
-    temp_min = []
-    temp_max = []
-    forecaster = mgr.forecast_at_place(place, '3h')
-    forecast=forecaster.forecast
-    for weather in forecast:
-        day=datetime.utcfromtimestamp(weather.reference_time())
-        #day = gmt_to_eastern(weather.reference_time())
-        date = day.date()
-        if date not in dates:
-            dates.append(date)
-            temp_min.append(None)
-            temp_max.append(None)
-            days.append(date)
-        temperature = weather.temperature(unit_c)['temp']
-        if not temp_min[-1] or temperature < temp_min[-1]:
-            temp_min[-1] = temperature
-        if not temp_max[-1] or temperature > temp_max[-1]:
-            temp_max[-1] = temperature
-    return(days, temp_min, temp_max)
-
-def init_plot():
-     plt.figure('PyOWM Weather', figsize=(5,4))
-     plt.xlabel('Day')
-     plt.ylabel(f'Temperature ({degree_sign}F)')
-     plt.title('Weekly Forecast')
-
-
-
-def plot_temperatures(days, temp_min, temp_max):
-    # days = dates.date2num(days)
-    fig = go.Figure(
-        data=[
-            go.Bar(name='minimum temperatures', x=days, y=temp_min),
-            go.Bar(name='maximum temperatures', x=days, y=temp_max)
-        ]
-    )
-    fig.update_layout(barmode='group')
-    return fig
-
-
-def plot_temperatures_line(days, temp_min, temp_max):
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(x=days, y=temp_min, name='minimum temperatures'))
-    fig.add_trace(go.Scatter(x=days, y=temp_max, name='maximimum temperatures'))
-    return fig
-
-def label_xaxis(days):
-    plt.xticks(days)
-    axes = plt.gca()
-    xaxis_format = dates.DateFormatter('%m/%d')
-    axes.xaxis.set_major_formatter(xaxis_format)
-
-def draw_bar_chart():
-    days, temp_min, temp_max = get_temperature()
-    fig = plot_temperatures(days, temp_min, temp_max)
-    # write_temperatures_on_bar_chart(bar_min, bar_max)
-    st.plotly_chart(fig)
-    st.title("Minimum and Maximum Temperatures")
-    for i in range (0,5):
-        st.write("### ",temp_min[i],degree_sign,' --- ',temp_max[i],degree_sign)
-
-
-def draw_line_chart():
-    days, temp_min, temp_max = get_temperature()
-    fig = plot_temperatures_line(days, temp_min, temp_max)
-    st.plotly_chart(fig)
-    st.title("Minimum and Maximum Temperatures")
-    for i in range (0,5):
-        st.write("### ",temp_min[i],degree_sign,' --- ',temp_max[i],degree_sign)
-
-def other_weather_updates():
-    forecaster = mgr.forecast_at_place(place, '3h')
-    st.title("Impending Temperature Changes :")
-    if forecaster.will_have_fog():
-        st.write("### FOG Alert!")
-    if forecaster.will_have_rain():
-        st.write("### Rain Alert")
-    if forecaster.will_have_storm():
-        st.write("### Storm Alert!")
-    if forecaster.will_have_snow():
-        st.write("### Snow Alert!")
-    if forecaster.will_have_tornado():
-        st.write("### Tornado Alert!")
-    if forecaster.will_have_hurricane():
-        st.write("### Hurricane Alert!")
-    if forecaster.will_have_clouds():
-        st.write("### Cloudy Skies")    
-    if forecaster.will_have_clear():
-        st.write("### Clear Weather!")
-
-def cloud_and_wind():
-    obs=mgr.weather_at_place(place)
-    weather=obs.weather
-    cloud_cov=weather.clouds
-    winds=weather.wind()['speed']
-    st.title("Cloud coverage and wind speed")
-    st.write('### The current cloud coverage for',place,'is',cloud_cov,'%')
-    st.write('### The current wind speed for',place, 'is',winds,'mph')
-
-def sunrise_and_sunset():
-    obs=mgr.weather_at_place(place)
-    weather=obs.weather
-    st.title("Sunrise and Sunset Times :")
-    india = pytz.timezone("Asia/Kolkata")
-    ss=weather.sunset_time(timeformat='iso')
-    sr=weather.sunrise_time(timeformat='iso')  
-    st.write("### Sunrise time in",place,"is",sr)
-    st.write("### Sunset time in",place,"is",ss)
-
-def updates():
-    other_weather_updates()
-    cloud_and_wind()
-    sunrise_and_sunset()
-
-
-if __name__ == '__main__':
+    temp_unit=" °F"
     
-    if st.button("SUBMIT"):
-        if g_type == 'Line Graph':
-            draw_line_chart()    
+if speed=="Kilometre/hour":
+    wind_unit=" km/h"
+else:
+    wind_unit=" m/s"
+
+api="9b833c0ea6426b70902aa7a4b1da285c"
+url=f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api}"
+response=requests.get(url)
+x=response.json()
+    
+if(st.button("SUBMIT")):
+    try:
+        lon=x["coord"]["lon"]
+        lat=x["coord"]["lat"]
+        ex="current,minutely,hourly"
+        url2=f'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={ex}&appid={api}'
+        res=requests.get(url2)
+        y=res.json()
+
+        maxtemp=[]
+        mintemp=[]
+        pres=[]
+        humd=[]
+        wspeed=[]
+        desc=[]
+        cloud=[]
+        rain=[]
+        dates=[]
+        sunrise=[]
+        sunset=[]
+        cel=273.15
+        
+        for item in y["daily"]:
+            
+            if unit=="Celsius":
+                maxtemp.append(round(item["temp"]["max"]-cel,2))
+                mintemp.append(round(item["temp"]["min"]-cel,2))
+            else:
+                maxtemp.append(round((((item["temp"]["max"]-cel)*1.8)+32),2))
+                mintemp.append(round((((item["temp"]["min"]-cel)*1.8)+32),2))
+
+            if wind_unit=="m/s":
+                wspeed.append(str(round(item["wind_speed"],1))+wind_unit)
+            else:
+                wspeed.append(str(round(item["wind_speed"]*3.6,1))+wind_unit)
+
+            pres.append(item["pressure"])
+            humd.append(str(item["humidity"])+' %')
+            
+            cloud.append(str(item["clouds"])+' %')
+            rain.append(str(int(item["pop"]*100))+'%')
+
+            desc.append(item["weather"][0]["description"].title())
+
+            d1=datetime.date.fromtimestamp(item["dt"])
+            dates.append(d1.strftime('%d %b'))
+            
+            sunrise.append( datetime.datetime.utcfromtimestamp(item["sunrise"]).strftime('%H:%M'))
+            sunset.append( datetime.datetime.utcfromtimestamp(item["sunset"]).strftime('%H:%M'))
+
+        def bargraph():
+            fig=go.Figure(data=
+                [
+                go.Bar(name="Maximum",x=dates,y=maxtemp,marker_color='crimson'),
+                go.Bar(name="Minimum",x=dates,y=mintemp,marker_color='navy')
+                ])
+            fig.update_layout(xaxis_title="Dates",yaxis_title="Temperature",barmode='group',margin=dict(l=70, r=10, t=80, b=80),font=dict(color="white"))
+            st.plotly_chart(fig)
+        
+        def linegraph():
+            fig = go.Figure()
+            fig.add_trace(go.Scatter(x=dates, y=mintemp, name='Minimum '))
+            fig.add_trace(go.Scatter(x=dates, y=maxtemp, name='Maximimum ',marker_color='crimson'))
+            fig.update_layout(xaxis_title="Dates",yaxis_title="Temperature",font=dict(color="white"))
+            st.plotly_chart(fig)
+            
+        icon=x["weather"][0]["icon"]
+        current_weather=x["weather"][0]["description"].title()
+        
+        if unit=="Celsius":
+            temp=str(round(x["main"]["temp"]-cel,2))
         else:
-            draw_bar_chart()
-        updates()
+            temp=str(round((((x["main"]["temp"]-cel)*1.8)+32),2))
+        
+        col1, col2 = st.columns(2)
+        with col1:
+            st.write("## Current Temperature ")
+        with col2:
+            st.image(f"http://openweathermap.org/img/wn/{icon}@2x.png",width=70)
+
+        
+        col1, col2= st.columns(2)
+        col1.metric("TEMPERATURE",temp+temp_unit)
+        col2.metric("WEATHER",current_weather)
+        st.subheader(" ")
+        
+        if graph=="Bar Graph":
+            bargraph()
+            
+        elif graph=="Line Graph":
+            linegraph()
+
+         
+        table1=go.Figure(data=[go.Table(header=dict(
+                  values = [
+                  '<b>DATES</b>',
+                  '<b>MAX TEMP<br>(in'+temp_unit+')</b>',
+                  '<b>MIN TEMP<br>(in'+temp_unit+')</b>',
+                  '<b>CHANCES OF RAIN</b>',
+                  '<b>CLOUD COVERAGE</b>',
+                  '<b>HUMIDITY</b>'],
+                  line_color='black', fill_color='royalblue',  font=dict(color='white', size=14),height=32),
+        cells=dict(values=[dates,maxtemp,mintemp,rain,cloud,humd],
+        line_color='black',fill_color=['paleturquoise',['palegreen', '#fdbe72']*7], font_size=14,height=32
+            ))])
+
+        table1.update_layout(margin=dict(l=10,r=10,b=10,t=10),height=328)
+        st.write(table1)
+        
+        table2=go.Figure(data=[go.Table(columnwidth=[1,2,1,1,1,1],header=dict(values=['<b>DATES</b>','<b>WEATHER CONDITION</b>','<b>WIND SPEED</b>','<b>PRESSURE<br>(in hPa)</b>','<b>SUNRISE<br>(in UTC)</b>','<b>SUNSET<br>(in UTC)</b>']
+                  ,line_color='black', fill_color='royalblue',  font=dict(color='white', size=14),height=36),
+        cells=dict(values=[dates,desc,wspeed,pres,sunrise,sunset],
+        line_color='black',fill_color=['paleturquoise',['palegreen', '#fdbe72']*7], font_size=14,height=36))])
+        
+        table2.update_layout(margin=dict(l=10,r=10,b=10,t=10),height=360)
+        st.write(table2)
+        
+        st.header(' ')
+        st.header(' ')
+        st.markdown("Made with :heart: by : ")
+        st.markdown("Shisu 🤩 && Mishra 🤗 && Gurushiksha 🥰")
+ 
+    except KeyError:
+        st.error(" Invalid city!!  Please try again !!")
